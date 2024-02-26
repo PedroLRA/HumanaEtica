027db3b8ac164820a4926eb95b00629f832463e1
Ricardo Espadinha <ricardo.miguel.espadinha@tecnico.ulisboa.pt>
2024-02-26 16:37:30 +0000

    feat: implemented first version of ParticipationRepository

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/repository/ParticipationRepository.java
@@ -0,0 +1,21 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain.Participation;
+
+import java.util.List;
+import java.util.Set;
+
+@Repository
+@Transactional
+public interface ParticipationRepository extends JpaRepository<Participation, Integer> {
+    @Query("SELECT a FROM Participation a WHERE a.activity.id = :activityId")
+    List<Participation> getParticipationsByActivityId(Integer activityId);
+
+}
+
+
