31207c3eb46beade1a0704d1ec5c924157df9d5d
Guilherme Maia <guilherme.tavares.maia@tecnico.ulisboa.pt>
2024-03-06 23:50:31 +0000

    feat: CreateParticipation on service and controller

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/ParticipationController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/ParticipationController.java
@@ -2,7 +2,11 @@ package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto.ParticipationDto;
 
 import java.security.Principal;
@@ -11,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/participations")
 public class ParticipationController {
+    
     @Autowired
     private ParticipationService participationService;
 
@@ -18,6 +23,14 @@ public class ParticipationController {
     @PreAuthorize("hasRole('ROLE_MEMBER') and hasPermission(#activityId, 'ACTIVITY.MEMBER')")
     public List<ParticipationDto>  getActivityParticipations(@PathVariable Integer activityId) {
         return participationService.getParticipationsByActivity(activityId);
+    }
+
+
+    @PostMapping("/{activityId}")
+    @PreAuthorize("(hasRole('ROLE_MEMBER'))")
+    public ParticipationDto createParticipation(Principal principal,@PathVariable Integer activityId, @Valid @RequestBody ParticipationDto participationDto) {
+        int userId = ((AuthUser) ((Authentication) principal).getPrincipal()).getUser().getId();
+        return participationService.createParticipation(userId, activityId, participationDto);
     }
 
 }
--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/ParticipationService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/ParticipationService.java
@@ -1,5 +1,8 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation;
 
+import static pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage.ACTIVITY_NOT_FOUND;
+import static pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage.USER_NOT_FOUND;
+
 import java.util.Comparator;
 import java.util.List;
 
@@ -8,13 +11,28 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.repository.ActivityRepository;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain.Participation;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto.ParticipationDto;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.repository.ParticipationRepository;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Member;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.repository.UserRepository;
 
 @Service
 public class ParticipationService {
+
     @Autowired
     ParticipationRepository participationRepository;
+
+    @Autowired
+    ActivityRepository activityRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
 
     @Transactional(isolation = Isolation.READ_COMMITTED)
     public List<ParticipationDto> getParticipationsByActivity(Integer activityId) {
@@ -23,6 +41,27 @@ public class ParticipationService {
                 .map(participation -> new ParticipationDto(participation))
                 .sorted(Comparator.comparingInt(ParticipationDto::getId))
                 .toList();
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public ParticipationDto createParticipation(Integer userId, Integer activityId, ParticipationDto participationDto) {
+        if (userId == null)
+            throw new HEException(USER_NOT_FOUND);
+        if (activityId == null)
+            throw new HEException(ACTIVITY_NOT_FOUND);
+        
+        Volunteer volunteer = (Volunteer) userRepository.findById(userId)
+            .orElseThrow(() -> new HEException(USER_NOT_FOUND, userId));
+
+        
+        Activity activity = activityRepository.findById(activityId)
+            .orElseThrow(() -> new HEException(ACTIVITY_NOT_FOUND, activityId));
+        
+            Participation participation = new Participation(activity, volunteer, participationDto);
+        
+            participationRepository.save(participation);
+
+        return new ParticipationDto(participation);
     }
 
 
