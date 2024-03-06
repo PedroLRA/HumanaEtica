d724eb5f1fd7513447bf734ae34365bfa2577818
Sebastiao Sousa <sebastiao.sousa@tecnico.ulisboa.pt>
2024-03-06 17:00:37 +0000

    fix: changed the constructor used on domain tests to the one required

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/domain/CreateAssessmentOneToOneInvariantTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/domain/CreateAssessmentOneToOneInvariantTest.groovy
@@ -2,10 +2,12 @@ package pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.domain
 
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.SpockTest
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.dto.AssessmentDto
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.institution.domain.Institution
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler
 
 import java.time.LocalDateTime
 
@@ -13,16 +15,24 @@ class CreateAssessmentOneToOneInvariantTest extends SpockTest {
 
     Institution institution
     Volunteer volunteer
-    Activity activityCompleted
+    Activity activity
+    AssessmentDto assessmentDto
 
     def setup() {
         institution = new Institution()
+
         volunteer = Mock()
         volunteer.getId() >> VOLUNTEER_ID_1
-        activityCompleted = Mock()
-        activityCompleted.getEndingDate() >> LocalDateTime.now().minusDays(1)
-        activityCompleted.getInstitution() >> institution
-        institution.addActivity(activityCompleted)
+
+        activity = Mock()
+        activity.getEndingDate() >> LocalDateTime.now().minusDays(1)
+
+        institution.addActivity(activity)
+
+        assessmentDto = Mock()
+        assessmentDto.getId() >> ASSESSMENT_ID_1
+        assessmentDto.getReview() >> REVIEW
+        assessmentDto.getReviewDate() >> DateHandler.toISOString(NOW)
     }
 
     def "volunteer already assessed this institution"() {
@@ -30,11 +40,12 @@ class CreateAssessmentOneToOneInvariantTest extends SpockTest {
         Assessment assessment = Mock()
         assessment.getVolunteer() >> volunteer
         assessment.getInstitution() >> institution
+
         institution.addAssessment(assessment)
         volunteer.addAssessment(assessment)
 
         when:
-        Assessment duplicateAssessment = new Assessment(REVIEW, NOW, institution, volunteer)
+        Assessment duplicateAssessment = new Assessment(institution, volunteer, assessmentDto)
 
         then:
         HEException heException = thrown(HEException)
@@ -45,15 +56,16 @@ class CreateAssessmentOneToOneInvariantTest extends SpockTest {
         given:
         Institution otherInstitution = Mock()
         otherInstitution.getAssessments() >> []
-        otherInstitution.getActivities() >> [activityCompleted]
-        Assessment assessment = new Assessment(REVIEW, NOW, otherInstitution, volunteer)
+        otherInstitution.getActivities() >> [activity]
+        Assessment assessment = new Assessment(otherInstitution, volunteer, assessmentDto)
 
         when:
-        Assessment validAssessment = new Assessment(REVIEW, NOW, institution, volunteer)
+        Assessment validAssessment = new Assessment(institution, volunteer, assessmentDto)
 
         then:
         notThrown(Exception)
 
+        validAssessment.getId() == ASSESSMENT_ID_1
         validAssessment.getReview() == REVIEW
         validAssessment.getReviewDate() == NOW
         validAssessment.getInstitution() == institution
@@ -65,14 +77,15 @@ class CreateAssessmentOneToOneInvariantTest extends SpockTest {
         given:
         Volunteer otherVolunteer = Mock()
         otherVolunteer.getId() >> VOLUNTEER_ID_2
-        Assessment assessment = new Assessment(REVIEW, NOW, institution, otherVolunteer)
+        Assessment assessment = new Assessment(institution, otherVolunteer, assessmentDto)
 
         when:
-        Assessment validAssessment = new Assessment(REVIEW, NOW, institution, volunteer)
+        Assessment validAssessment = new Assessment(institution, volunteer, assessmentDto)
 
         then:
         notThrown(Exception)
 
+        validAssessment.getId() == ASSESSMENT_ID_1
         validAssessment.getReview() == REVIEW
         validAssessment.getReviewDate() == NOW
         validAssessment.getInstitution() == institution
@@ -83,18 +96,21 @@ class CreateAssessmentOneToOneInvariantTest extends SpockTest {
         given:
         Volunteer otherVolunteer = Mock()
         otherVolunteer.getId() >> 2
+
         Institution otherInstitution = Mock()
         otherInstitution.getAssessments() >> []
-        otherInstitution.getActivities() >> [activityCompleted]
-        Assessment assessment = new Assessment(REVIEW, NOW, institution, otherVolunteer)
-        Assessment otherAssessment = new Assessment(REVIEW, NOW, otherInstitution, volunteer)
+        otherInstitution.getActivities() >> [activity]
+
+        Assessment assessment = new Assessment(institution, otherVolunteer, assessmentDto)
+        Assessment otherAssessment = new Assessment(otherInstitution, volunteer, assessmentDto)
 
         when:
-        Assessment validAssessment = new Assessment(REVIEW, NOW, institution, volunteer)
+        Assessment validAssessment = new Assessment(institution, volunteer, assessmentDto)
 
         then:
         notThrown(Exception)
 
+        validAssessment.getId() == ASSESSMENT_ID_1
         validAssessment.getReview() == REVIEW
         validAssessment.getReviewDate() == NOW
         validAssessment.getInstitution() == institution
