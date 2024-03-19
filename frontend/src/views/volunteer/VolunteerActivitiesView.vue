43831fedcca86d237cf443a6f8d571d68d3678a5
Pedro Luís Rodrigues de Almeida <pedrolralmeida@tecnico.ulisboa.pt>
2024-03-19 09:27:31 +0000

    feat: added "apply for activity" button
    Closes: #60

--- frontend/src/views/volunteer/VolunteerActivitiesView.vue
+++ frontend/src/views/volunteer/VolunteerActivitiesView.vue
@@ -40,6 +40,19 @@
             </template>
             <span>Report Activity</span>
           </v-tooltip>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-icon
+                class="mr-2 action-button"
+                color="blue"
+                v-on="on"
+                data-cy="enrollButton"
+                @click="newEnrollment()"
+                >mdi-arrow-right-bold-circle</v-icon
+              >
+            </template>
+            <span>Apply for Activity</span>
+          </v-tooltip>
           <v-tooltip v-if="satisfiesInvariants(item)" bottom>
             <template v-slot:activator="{ on }">
               <v-icon
@@ -187,6 +200,11 @@ export default class VolunteerActivitiesView extends Vue {
         await this.$store.dispatch('error', error);
       }
     }
+  }
+
+  newEnrollment() {
+    //Call dialog and other function to create enrollment
+    console.log('click');
   }
 
   newAssessment(activity: Activity) {
