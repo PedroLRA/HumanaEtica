e8b681c71922782fa627164159faded63017867d
1100070 <ricardo.miguel.espadinha@tecnico.ulisboa.pt>
2024-03-26 14:46:42 +0000

    feat: added test to check if first activity has 2 enrollments

--- frontend/tests/e2e/specs/member/member.js
+++ frontend/tests/e2e/specs/member/member.js
@@ -38,5 +38,13 @@ describe('Volunteer', () => {
     cy.get('[data-cy="memberActivitiesTable"] tbody tr')
         .eq(0).children().eq(3).should('contain', 1)
 
+    //first activity has 2 enrollments
+    cy.get('[data-cy="memberActivitiesTable"] tbody tr')
+        .find('[data-cy="showEnrollments"]')
+        .eq(0) // Select the first button
+        .click(); // Click on it
+
+    cy.get('[data-cy="activityEnrollmentsTable"] tbody tr')
+        .should('have.length', 2)
   });
 });
