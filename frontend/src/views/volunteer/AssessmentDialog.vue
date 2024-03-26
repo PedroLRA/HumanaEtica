8ba714d97f0386566281f44a0239a9824fc34870
Sebastiao Sousa <sebastiao.sousa@tecnico.ulisboa.pt>
2024-03-26 14:18:39 +0000

    fix: AssessmentDialog format now matches required format.
    Width reduced from 1300 to 600, text box occupies whole row, save button only appears on 10th character (instead of being there but inactive)

--- frontend/src/views/volunteer/AssessmentDialog.vue
+++ frontend/src/views/volunteer/AssessmentDialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog v-model="dialog" persistent width="1300">
+  <v-dialog v-model="dialog" persistent width="600">
     <v-card>
       <v-card-title>
         <span class="headline">
@@ -9,15 +9,13 @@
       <v-card-text>
         <v-form ref="form" lazy-validation>
           <v-row>
-            <v-col cols="12" md="4" sm="6">
-              <v-text-field
-                v-model="newAssessment.review"
-                :rules="[(v) => !!v || 'Review is required']"
-                data-cy="reviewInput"
-                label="*Review"
-                required
-              ></v-text-field>
-            </v-col>
+            <v-text-field
+              v-model="newAssessment.review"
+              :rules="[(v) => !!v || 'Review is required']"
+              data-cy="reviewInput"
+              label="*Review"
+              required
+            ></v-text-field>
           </v-row>
         </v-form>
       </v-card-text>
@@ -31,7 +29,7 @@
           Close
         </v-btn>
         <v-btn
-          :disabled="!canSave"
+          v-if="canSave"
           color="blue-darken-1"
           data-cy="saveAssessment"
           variant="text"
