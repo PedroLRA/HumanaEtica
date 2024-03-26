1f7884e77a8f4474cd89cfe05279e2d087ececc0
Rodrigo Dias <rodrigofdias@tecnico.ulisboa.pt>
2024-03-26 20:29:16 +0000

    feat: Test e2e - Volunteer - Check first activity in table has name A1

--- frontend/tests/e2e/specs/assessment/assessment.js
+++ frontend/tests/e2e/specs/assessment/assessment.js
@@ -27,4 +27,18 @@ describe('Assessment', () => {
             .children()
             .should('have.length', 10)
     });
+
+    it('volunteer activity table first element has name \"A1\"', () => {
+        // intercept get assessments
+        cy.intercept('GET', '/users/getAssessments').as('getAssessments');
+
+        cy.get('[data-cy="volunteerActivities"').click();
+        cy.wait('@getAssessments');
+
+        // check results
+        cy.get('[data-cy="volunteerActivitiesTable"] tbody tr')
+            .should('have.length', 1)
+        cy.get('[data-cy="volunteerActivitiesTable"] tbody tr')
+            .eq(0).children().eq(0).should('contain', 'A1')
+    });
 });
